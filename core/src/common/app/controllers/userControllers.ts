fdc6b326e7a67669d2ea28b6e564a956a7816454
Karim Ahmed <55539101+KarimAhmedAhmed@users.noreply.github.com>
2023-09-05 23:24:07 +0300

    Update userControllers.ts

--- core/src/common/app/controllers/userControllers.ts
+++ core/src/common/app/controllers/userControllers.ts
@@ -1,30 +1,22 @@
 // controllers/orderController.ts
-import { Request, Response } from 'express';
-import { Auth } from '../../domain/useCases/Auth'
-import UserModel from '../infrastructure/data-access/models/user.model';
-import UserRepository, { UserRepositoryMongo } from '../infrastructure/data-access/repositories/UserRepository';
-import { userRole } from '../infrastructure/data-access/dtos/UserDTO';
-import { wrapper } from '../utils/Wrapper';
-
+import { Request, Response } from "express";
+import { Auth } from "../../domain/useCases/Auth";
+import { UserRepositoryMongo } from "../infrastructure/data-access/repositories/UserRepository";
+import { userRole } from "../infrastructure/data-access/dtos/UserDTO";
+import { wrapper } from "../utils/Wrapper";
 
 let userRepository = new UserRepositoryMongo();
 let auth = new Auth(userRepository);
 
-export const register = wrapper( async (req: Request, res: Response) => {
-    const {role} = req.params; 
-    const { username, password } = req.body;
-    const user = await auth.register(username, password, role as userRole);
-    res.status(201).json(user);
-})
+export const register = wrapper(async (req: Request, res: Response) => {
+  const { role } = req.params;
+  const { username, password } = req.body;
+  const user = await auth.register(username, password, role as userRole);
+  res.status(201).json(user);
+});
 
-
-export const login = async (req: Request, res: Response) => {
-try {
-    const { username, password } = req.body;
-    const user = await auth.login(username,password)
-    res.status(201).json(user);
-} catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-}
-}
-
+export const login = wrapper(async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await auth.login(username, password);
+  res.status(201).json(user);
+});
